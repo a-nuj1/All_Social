0c5669cb3c18e6426293b0f387a8bc16cda9c885
Anuj <anujgupta1532003@gmail.com>
2023-07-05 01:10:23 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = 7000;
+
+app.use('/', require('./routes'));
+
+
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is Running on port: ${port}`);
+});
